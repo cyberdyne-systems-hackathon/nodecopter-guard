b54404961f26956cf6656dd28927c0190e08ffaa
“Richard <poolieweb@yahoo.co.uk>
2015-03-27 18:22:44 +0000

    Cleaned

--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
 var df = require('dateformat')
   , autonomy = require('../node_modules/ardrone-autonomy')
   , mission  = autonomy.createMission()
-  , arDrone = require('../nodecopter/node_modules/ar-drone')
-  , arDroneConstants = require('../nodecopter/node_modules/ar-drone/lib/constants')
-  ;
-
-function navdata_option_mask(c) {
-  return 1 << c;
-}
-
-// From the SDK.
-var navdata_options = (
-    navdata_option_mask(arDroneConstants.options.DEMO)
-  | navdata_option_mask(arDroneConstants.options.VISION_DETECT)
-  | navdata_option_mask(arDroneConstants.options.MAGNETO)
-  | navdata_option_mask(arDroneConstants.options.WIFI)
-);
-
-// Connect and configure the drone
-mission.client().config('general:navdata_demo', true);
-mission.client().config('general:navdata_options', navdata_options);
-mission.client().config('video:video_channel', 1);
-mission.client().config('detect:detect_type', 12);
-
-mission.log("mission-" + df(new Date(), "yyyy-mm-dd_hh-MM-ss") + ".txt");
 
 mission.takeoff()
        .go({x:0, y:0, z:1.5})
