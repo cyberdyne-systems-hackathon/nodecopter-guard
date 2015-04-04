dd7628d8368ebfe285d5556caa1d25ce07e3a720
“Richard <rich@Richards-Air.home>
2015-04-04 18:07:23 +0100

    Update ignore file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
+var arDrone = require('../node_modules/ar-drone');
+var arDroneConstants = require('../node_modules/ar-drone/lib/constants');
+var autonomy = require('../node_modules/ardrone-autonomy');
 
-
-var df = require('dateformat')
-
-
-var arDrone = require('../node_modules/ar-drone')
-var arDroneConstants = require('../node_modules/ar-drone/lib/constants')
-
-var autonomy = require('../node_modules/ardrone-autonomy')
-
-var http = require("http"),
-    drone = require("../node_modules/dronestream");
+var http = require("http");
+var drone = require("../node_modules/dronestream");
 
 var client  = arDrone.createClient();
-
-
 
 
 function navdata_option_mask(c) {
@@ -30,18 +22,12 @@ var navdata_options = (
 
 
 // Connect and configure the drone
-
 client.config('general:navdata_demo', true);
 client.config('general:navdata_options', navdata_options);
 client.config('video:video_channel', 1);
 client.config('detect:detect_type', 12);
-client.config('control:flying_node',2)
 
-
-
-
-
-client.config()
+client.config();
 
 var control = new autonomy.Controller(client, {debug: false});
 var mission = new autonomy.Mission(client, control, {debug: false});
@@ -54,25 +40,6 @@ var server = http.createServer(function(req, res) {
 drone.listen(server);
 server.listen(5555);
 
-
-
-mission.takeoff()
-       .altitude(1.75)
-       .hover(200)
-        // .cw(4)
-       // .altitude(1.4)
-       // .forward(0.5)
-       // .right(0.5)
-       // .backward(0.5)
-       .forward(3.6)
-       // .hover(1000)
-       // .hover(500)
-        .hover(200)
-
-        .cw(180)
-         .hover(200)
-          .forward(3.2)
-       .land();
 
 mission.run(function (err, result) {
     if (err) {
